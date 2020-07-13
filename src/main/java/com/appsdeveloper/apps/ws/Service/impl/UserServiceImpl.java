0646fda60312e9415dc7122572a2f6781bbbe788
spratibha08 <spratibha@gmail.com>
2020-07-13 13:56:11 +0530

    Wokring code changes till module7 - chapter 37

--- src/main/java/com/appsdeveloper/apps/ws/Service/impl/UserServiceImpl.java
+++ src/main/java/com/appsdeveloper/apps/ws/Service/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import com.appsdeveloper.apps.ws.UserRepository;
 import com.appsdeveloper.apps.ws.Service.UserService;
 import com.appsdeveloper.apps.ws.io.entity.UserEntity;
+import com.appsdeveloper.apps.ws.shared.Utils;
 import com.appsdeveloper.apps.ws.shared.dto.UserDto;
 
 @Service
@@ -15,14 +16,20 @@ public class UserServiceImpl implements UserService {
 	@Autowired
 	UserRepository userRepository;
 	
+	@Autowired
+	Utils utils;
+	
 	@Override
 	public UserDto createUser(UserDto user) {
-
+		
+		if(userRepository.findByEmail(user.getEmail()) != null) throw new RuntimeException("Record already exists");
+		
 		UserEntity userEntity = new UserEntity();
 		BeanUtils.copyProperties(user, userEntity);
 		
+		String publicUserId = utils.generateUserId(30);
+		userEntity.setUserId(publicUserId);
 		userEntity.setEncryptedPassword("test");
-		userEntity.setUserId("testUserId");
 		
 		UserEntity storedUserDetails = userRepository.save(userEntity);
 		
