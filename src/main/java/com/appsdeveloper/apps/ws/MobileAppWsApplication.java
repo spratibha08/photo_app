1395725bd4c5b03293181ae119776471d6cc2be7
spratibha08 <spratibha@gmail.com>
2020-07-14 00:34:33 +0530

    code runs successfully till Section 8 - chapter 40

--- src/main/java/com/appsdeveloper/apps/ws/MobileAppWsApplication.java
+++ src/main/java/com/appsdeveloper/apps/ws/MobileAppWsApplication.java
@@ -2,6 +2,8 @@ package com.appsdeveloper.apps.ws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class MobileAppWsApplication {
@@ -13,6 +15,11 @@ public class MobileAppWsApplication {
 		// test comment - for jenkins job test
 		
 		// test practiceGit1
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
