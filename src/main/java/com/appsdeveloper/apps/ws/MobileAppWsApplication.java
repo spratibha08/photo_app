07572a2bcc4be98e5d80eefe6bf283313168d5b9
spratibha08 <spratibha@gmail.com>
2020-07-14 18:59:34 +0530

    working fine till Section9-Chapter51

--- src/main/java/com/appsdeveloper/apps/ws/MobileAppWsApplication.java
+++ src/main/java/com/appsdeveloper/apps/ws/MobileAppWsApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import com.appsdeveloper.apps.ws.security.AppProperties;
+
 @SpringBootApplication
 public class MobileAppWsApplication {
 
@@ -26,6 +28,12 @@ public class MobileAppWsApplication {
 	public SpringApplicationContext springApplicationContext()
 	{
 		return new SpringApplicationContext();
+	}
+	
+	@Bean(name="AppProperties")
+	public AppProperties getAppProperties()
+	{
+		return new AppProperties();
 	}
 	
 }
